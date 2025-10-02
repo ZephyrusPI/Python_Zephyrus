87567c0149119ac6997807e9a7a8a90cc6dd42fa
GustavoAlvesDeOliveira <gustavo.aoliveira@sptech.school>
2025-10-02 13:11:34 -0300

    Adicionando identificadores

--- Captura.py
+++ Captura.py
@@ -6,7 +6,9 @@ from datetime import datetime
 
 dados = {
     "timestamp": [],
-    "usuario": [],
+    "ID": [],
+    "Modelo": [],
+    "Area": [],
     "CPU": [],
     "RAM": [],
     "Disco": [],
@@ -18,15 +20,23 @@ dados = {
 def obter_uso():
     # usuario = ps.users()
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    user = "Ronaldo" #seu nome aqui
+    id = 0 #ID 
+    modelo = "VENTILADOR2000" #modelo do ventilador
+    area = "UTI" #Digite a área
+    possuiBateria = True #digite se o modelo tem bateria
     cpuPercent = ps.cpu_percent(interval=1)
     RAM = ps.virtual_memory()
     disco = ps.disk_usage('/')
     processos = len({p.pid: p.info for p in ps.process_iter(['name', 'username'])})
-    bateria = ps.sensors_battery()
+    if(possuiBateria == True):
+        bateria = ps.sensors_battery()
+    else:
+        bateria == None
 
     dados["timestamp"].append(timestamp)
-    dados["usuario"].append(user)
+    dados["ID"].append(id)
+    dados["Modelo"].append(modelo)
+    dados["Area"].append(area)
     dados["CPU"].append(cpuPercent)
     dados["RAM"].append(round(RAM.used / 1024 ** 3))
     dados["Disco"].append(round(disco.used / 1024 ** 3))
@@ -46,7 +56,7 @@ def monitoramento():
         while True:
             obter_uso()
 
-            print(f"\nData/Hora: {dados['timestamp'][-1]} \nUsuário: {dados["usuario"][-1]} \nUso da CPU: {dados["CPU"][-1]}% \nUso de RAM: {dados["RAM"][-1]} Gb \nDisco usado: {dados["Disco"][-1]} Gb \nProcessos: {dados["Processos"][-1]}\nBateria: {dados["Bateria"][-1]}%")
+            print(f"\nData/Hora: {dados['timestamp'][-1]} \nID: {dados["ID"][-1]}\nModelo: {dados["Modelo"][-1]}\nÁrea: aa{dados["Area"][-1]} \nUso da CPU: {dados["CPU"][-1]}% \nUso de RAM: {dados["RAM"][-1]} Gb \nDisco usado: {dados["Disco"][-1]} Gb \nProcessos: {dados["Processos"][-1]}\nBateria: {dados["Bateria"][-1]}%")
 
             salvar_csv()
             time.sleep(5)
