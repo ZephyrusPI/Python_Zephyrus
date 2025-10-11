5dae9d8068361cd111656ec8562fd2ad4049e178
LCP05 <lucas.previtero@sptech.school>
2025-10-11 15:35:26 -0300

    Alteração dos dados da máquina

--- Captura.py
+++ Captura.py
@@ -6,9 +6,7 @@ from datetime import datetime
 
 dados = {
     "timestamp": [],
-    "Area": [],
-    "Modelo": [],
-    "ID": [],
+    "nSerie": [],
     "CPU": [],
     "RAM": [],
     "Disco": [],
@@ -16,30 +14,25 @@ dados = {
     "Bateria":[]
 }
 
-
-area = "Cirurgia" #Digite a área
-modelo = "Dx3010" #modelo do ventilador
-id = 4 #ID do ventilador
+user = ps.users()
+nSerie = user[-1].name
+nSerie = "SN123456789" #No final, deve ser uma coleta de usuário da psutil, pois cada ventilador terá seu identificador no sistema, e no banco SQL
 
 
 def obter_uso():
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    possuiBateria = False #digite se o modelo tem bateria
     cpuPercent = ps.cpu_percent(interval=1)
     RAM = ps.virtual_memory()
     disco = ps.disk_usage('/')
     processos = len({p.pid: p.info for p in ps.process_iter(['name', 'username'])})
 
-    if(possuiBateria == True):
-        bateria = ps.sensors_battery()
-        bateria = round(bateria.percent,1)
-    else:
-        bateria = "N/A"
+    
+    bateria = ps.sensors_battery()
+    bateria = round(bateria.percent,1)
+    
 
     dados["timestamp"].append(timestamp)
-    dados["ID"].append(id)
-    dados["Modelo"].append(modelo)
-    dados["Area"].append(area)
+    dados["nSerie"].append(nSerie)
     dados["CPU"].append(cpuPercent)
     dados["RAM"].append(RAM.percent)
     dados["Disco"].append(round(disco.used / 1024 ** 3))
@@ -50,7 +43,7 @@ def obter_uso():
 
 def salvar_csv():
     df = pd.DataFrame(dados)
-    df.to_csv(f"coleta-{area}-{modelo}-{id}.csv", encoding="utf-8", index=False)
+    df.to_csv(f"coleta-{nSerie}.csv", encoding="utf-8", index=False)
 
 
 
@@ -59,7 +52,7 @@ def monitoramento():
         while True:
             obter_uso()
 
-            print(f"\nData/Hora: {dados['timestamp'][-1]} \nÁrea: {dados["Area"][-1]}\nModelo: {dados["Modelo"][-1]}\nID: {dados["ID"][-1]}\nUso da CPU: {dados["CPU"][-1]}% \nUso de RAM: {dados["RAM"][-1]}% \nDisco usado: {dados["Disco"][-1]} Gb \nProcessos: {dados["Processos"][-1]}\nBateria: {dados["Bateria"][-1]}%")
+            print(f"\nData/Hora: {dados['timestamp'][-1]}\nNúmero de série: {dados["nSerie"][-1]}\nUso da CPU: {dados["CPU"][-1]}% \nUso de RAM: {dados["RAM"][-1]}% \nDisco usado: {dados["Disco"][-1]} Gb \nProcessos: {dados["Processos"][-1]}\nBateria: {dados["Bateria"][-1]}%")
 
             salvar_csv()
             time.sleep(5)
